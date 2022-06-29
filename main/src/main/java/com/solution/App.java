9c2b0d188b765067b13c954e53cf15db757134d8
Rohith Kumar <srk240998@gmail.com>
2022-06-29 19:24:31 +0200

    SMOTE

--- main/src/main/java/com/solution/App.java
+++ main/src/main/java/com/solution/App.java
@@ -1,11 +1,15 @@
 package com.solution;
 
+import weka.attributeSelection.AttributeSelection;
+import weka.attributeSelection.InfoGainAttributeEval;
+import weka.attributeSelection.Ranker;
 import weka.core.Instances;
 import weka.filters.Filter;
 import weka.filters.unsupervised.attribute.ReplaceMissingValues;
-import weka.filters.unsupervised.attribute.ReplaceWithMissingValue;
-import weka.filters.unsupervised.attribute.Standardize;
+import weka.filters.unsupervised.instance.Randomize;
+import weka.filters.unsupervised.instance.RemovePercentage;
 import weka.core.converters.ConverterUtils.DataSource;
+import weka.filters.supervised.instance.SMOTE;
 
 import org.apache.commons.math.stat.correlation.*;
 
@@ -13,8 +17,8 @@ import org.apache.commons.math.stat.correlation.*;
 public class App 
 {
 
+    //Checking the co-relation co-efficient b/w the features(attributes)
     public static void checkCorrelation( Instances data){
-        //Checking the co-relation co-efficient b/w the features(attributes)
         int limit = data.numAttributes()-1;
         for(int i=1; i<limit; i++){
             for(int j=i+1; j<limit; j++){
@@ -28,20 +32,32 @@ public class App
             }
         }
     }
+
+    public static Instances[] trainTestSplit(Instances data) throws Exception {
+        RemovePercentage rp = new RemovePercentage();
+        rp.setInputFormat(data);
+        rp.setPercentage(30);
+        Instances trainSet = Filter.useFilter(data, rp);
+        rp = new RemovePercentage();
+        rp.setInputFormat(data);
+        rp.setPercentage(30);
+        rp.setInvertSelection(true);
+        Instances testSet = Filter.useFilter(data, rp);
+        return new Instances[] {trainSet, testSet};
+        
+    }
     public static void main( String[] args ) throws Exception
     {
         DataSource source = new DataSource("data.arff");
         Instances data = source.getDataSet();
 
-        
-
-        //Checking for String values
+        // //Checking for String values
         for(int i = 1; i<data.numAttributes()-1; i++){
             if(data.attribute(i).isString()){
                 System.out.println(i+" contains string values.");
             }
         }
-        //No String values found
+        // //No String values found
 
         // //Removing attributes with constant values
         // for(int i=1; i<data.numAttributes()-1; i++){
@@ -51,16 +67,26 @@ public class App
         data.deleteAttributeAt(9);
         data.deleteAttributeAt(7);
         data.deleteAttributeAt(4);
-        //Constant attributes removed!
+        // //Constant attributes removed!
 
         
-        //Checking for nominal features
+        // //Checking for nominal features
         for(int i = 1; i<data.numAttributes()-1; i++){
             if(data.attribute(i).isNominal()){
                 System.out.println(i+" is nominal.");
             }
         }
-        // No nominal features were found!
+        // //No nominal features were found!
+
+        // //Handling Null Values    
+        // for(int i=1; i<data.numAttributes()-1; i++){
+        //     System.out.println(i+" "+data.attributeStats(i));
+        // }
+        // //Null values are present in attribute(4)
+        ReplaceMissingValues replace = new ReplaceMissingValues();
+        replace.setInputFormat(data);
+        data = Filter.useFilter(data, replace);
+        // // Null values are replaced with mean
 
 
         // //Removing features(attributes) with high correlation
@@ -71,12 +97,55 @@ public class App
         data.deleteAttributeAt(16);
         data.deleteAttributeAt(10);
         // checkCorrelation(data);
-        //Highly correlated features removed
+        // //Highly correlated features removed
+        
+
+        // //Choosing attributes based on Information Gain(Dimensionality Reduction)
+        // //The threshold is selected based on the research work done by Prasetiyowati et. al 
+        // //(https://doi.org/10.1186/s40537-021-00472-4)  
+        InfoGainAttributeEval evaluator = new InfoGainAttributeEval();
+        Ranker search = new Ranker();
+        search.setOptions(new String[] { "-T", "0.028" });	// information gain threshold
+        AttributeSelection attSelect = new AttributeSelection();
+        attSelect.setEvaluator(evaluator);
+        attSelect.setSearch(search);
+        attSelect.setRanking(true);
+        attSelect.SelectAttributes(data);
+        data = attSelect.reduceDimensionality(data);
+        // System.out.println(attSelect.toResultsString());
+
+        // //class balance & sampling
+        // System.out.println(data.attributeStats(data.numAttributes()-1));
+        // // Class imbalance is noted; class 0: 275 & class 1: 122
+
+        // //Modelling
+        data.setClassIndex(data.numAttributes()-1);
+        double tp, fp, fn, tn = 0;
+        int kFolds = 10;
+        for(int k=0; k<kFolds; k++){
+            Randomize randomizer = new Randomize();
+            randomizer.setInputFormat(data);
+            randomizer.setRandomSeed(400);
+            data = Filter.useFilter(data, randomizer);
+            Instances[] splitData = trainTestSplit(data);
+            Instances trainSet = splitData[0];
+            Instances testSet = splitData[1];
+            // //Minority over sampling
+            SMOTE smote = new SMOTE();
+            smote.setInputFormat(trainSet);
+            trainSet = Filter.useFilter(trainSet, smote);
 
 
-        //replacing missing values
+        }
+        
 
-        //class balance & sampling
-        System.out.println(data.attributeStats(31));
+        
+
+        
+        // for(int i=0; i<fold; i++){
+
+
+        // }
+        
     }
 }
